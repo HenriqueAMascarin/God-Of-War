c0a43dc496d9fce9d40988556e3dc355e11a2f0b
LuckTheGamers <thegamers056@gmail.com>
2022-12-17 20:39:23 -0300

    text align in p and h2 changes

--- src/components/InfoSection.tsx
+++ src/components/InfoSection.tsx
@@ -9,7 +9,7 @@ export default function InfoSection(){
             
             <div className='info-godOfWar'>
                 <h1>Embarque com Kratos e Atreus em uma jornada épica emocionante 
-                sobre apego e superação
+                sobre apego e superação!
                 </h1>
                 <p>Do Santa Monica Studio, esta é a sequência da aclamada versão de 2018 de God of War. O Fimbulwinter já começou. Kratos e Atreus devem viajar pelos Nove Reinos em busca de respostas enquanto as forças asgardianas se preparam para uma batalha profetizada que causará o fim do mundo. Nessa jornada, eles explorarão paisagens míticas impressionantes e enfrentarão inimigos aterradores: deuses nórdicos e monstros. A ameaça do Ragnarök se aproxima. Kratos e Atreus terão de escolher entre a segurança deles próprios e a dos reinos.
                 </p>
